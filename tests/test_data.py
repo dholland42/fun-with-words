774775e2b4bd390f3e717075796e1e41cb9ebcbd
Daniel Holland <danielholland414@gmail.com>
2020-12-30 15:22:29 +0000

    starting tests

---
+++ tests/test_data.py
@@ -0,0 +1,15 @@
+"""Unit tests for parsing raw data."""
+
+import unittest
+
+# from fun_with_words.data import parse_book
+
+
+class TestDataParsing(unittest.TestCase):
+    """Test data parsing."""
+    def test_parse_book(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
