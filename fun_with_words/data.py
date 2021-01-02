e936c88ab130d9ce168b5ce301feddb9323813ec
Daniel Holland <danielholland414@gmail.com>
2021-01-02 21:09:20 +0000

    actually add data functions

--- fun_with_words/data.py
+++ fun_with_words/data.py
@@ -1,18 +1,58 @@
 """A module for parsing the raw dictionary file into structured data."""
 
-def parse_book(book):
-    words = dict()
-    word = ""
-    defn = []
-    first_complete = False
-    for row in book:
+import os
+import itertools
+import collections
+
+
+STARTLINE = 27
+ENDLINE = 973904
+
+def get_full_dataset(filename=os.path.join(os.path.abspath(__file__), "../data/dictionary.txt")):
+    with open(filename, encoding="utf-8") as f:
+    raw_data = f.readlines()[STARTLINE:ENDLINE]
+    words = defns(raw_data, lambda x: x[0].isalnum())
+    data = [(d, word) for (word, defn) in words.items() for d in defn]
+    return data
+
+
+def canonical_lines(raw_data):
+    current = ""
+    started = False
+    for row in raw_data:
         row = row.strip()
         if not row:
-            continue
+            if current:
+                yield current.strip()
+                current = ""
+        elif row.isupper():
+            yield row
+        else:
+            current += " " + row
+
+def get_pairs(data):
+    word = ""
+    defs = []
+    for row in data:
         if row.isupper():
-            words[word] = defn
+            if word:
+                for w in word.split(";"):
+                    if len(w.split()) == 1:
+                        yield w.strip(), defs
             word = row
-            defn = []
-            continue
-        defn.append(row.split(".")[0])
+            defs = []
+        elif row.lower().startswith("defn:"):
+            for d in row[5:].split(".")[0].split(";"):
+                if not d.strip().startswith("See "):
+                    defs.append(d.strip().lower())
+    for w in word.split(";"):
+        if len(w.split()) == 1:
+            yield w.strip(), defs
+
+def defns(raw_data, filter_func=lambda x: True):
+    words = collections.defaultdict(lambda : list())
+    for word, defn in filter(filter_func, get_pairs(canonical_lines(raw_data))):
+        if defn:
+            words[word.lower()].extend(defn)
     return words
+
